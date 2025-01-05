e5b9f5150dcb578ad319702cddf2e196044aa789
Muhammad Rafiq <rafkhan9323@gmail.com>
2025-01-05 20:49:57 +0500

    Add user existence check and improve error handling in registration

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -8,29 +8,39 @@ const registerUser = async (req, res) => {
   try {
     const jwtSecret = process.env.JWT_SECRET;
     const bcryptSalt = bcrypt.genSaltSync(10);
+
+    const existingUser = await UserModel.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
     const hashedPassword = bcrypt.hashSync(password, bcryptSalt);
 
     const createdUser = await UserModel.create({
       username: username,
       password: hashedPassword,
     });
+
     jwt.sign(
       { userId: createdUser._id, username },
       jwtSecret,
       {},
       (err, token) => {
         if (err) throw err;
+
         res
           .cookie("token", token, { sameSite: "none", secure: true })
           .status(201)
           .json({
             id: createdUser._id,
+            username: createdUser.username,
+            token,
           });
       }
     );
   } catch (error) {
-    if (error) throw error;
-    res.status(500).json("error");
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
 
