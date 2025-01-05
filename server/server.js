b3f5f1993e1a7a51067249d122e11613ae78c760
Muhammad Rafiq <rafkhan9323@gmail.com>
2025-01-05 20:50:22 +0500

    Add CORS middleware to server configuration

--- server/server.js
+++ server/server.js
@@ -3,11 +3,18 @@ import dotenv from "dotenv";
 import databaseConnection from "./config/database.js";
 import authRoute from "./routes/authRoute.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
