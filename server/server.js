449b6be7ecbe981790a3a2763882e3fe3df4354f
Muhammad Rafiq <rafkhan9323@gmail.com>
2025-01-07 19:58:02 +0500

    Add cookie parser middleware to server configuration

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ import cors from "cors";
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
